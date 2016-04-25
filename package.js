cf4da845f764ebfe344ebce0330160744ab995c2
Bartosz Wojtkowiak <bartosz@wojtkowiak.it>
2016-04-25 22:46:41 +0200

    v 1.0.3

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
     name: 'omega:monitor-widget',
-    version: '0.1.1',
+    version: '0.1.3',
     summary: 'Tiny draggable graph for presenting some basic app metrics.',
     git: 'https://github.com/wojtkowiak/meteor-monitor-widget',
     documentation: 'README.md'
